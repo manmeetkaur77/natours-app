8d5d6f710b7e96d478261b98c392fd7f959b1764
manmeetkaur77 <manmeetkaur1629@gmail.com>
2025-06-18 23:46:04 +0530

    generate tokens

--- controllers/authController.js
+++ controllers/authController.js
@@ -32,9 +32,9 @@ exports.login = catchAsync(async (req, res, next) => {
       return next(new AppError('Please provide email and password!', 400));
   
     // 2. Check if user exists & password is correct
-    // const user = await User.findOne({ email }).select('+password');
-    // if (!user || !(await user.correctPassword(password, user.password)))
-    //   return next(new AppError('Incorrect email or password', 401));
+    const user = await User.findOne({ email }).select('+password');
+    if (!user || !(await user.correctPassword(password, user.password)))
+      return next(new AppError('Incorrect email or password', 401));
   
     // 3. If everything ok, send token to client
     const token = jwt.sign({email:email}, process.env.JWT_SECRET, {
